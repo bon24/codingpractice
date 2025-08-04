5b25bbe84365233e35a3f46e5b3f1208d329d2dc
bon24 <gbk24@naver.com>
2025-08-04 15:06:31 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,9 @@
+def solution(binomial):
+    a = binomial.split()
+    if a[1]=="+":
+        answer = int(a[0])+int(a[2])
+    elif a[1]=="-":
+        answer = int(a[0])-int(a[2])
+    elif a[1]=="*":
+        answer = int(a[0])*int(a[2])
+    return answer
