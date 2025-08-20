7f46516a1c3caa19b47875ce1b1979386ca191b2
bon24 <gbk24@naver.com>
2025-08-20 20:07:46 +0900

    [level 0] Title: 그림 확대, Time: 0.10 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -0,0 +1,10 @@
+def solution(picture, k):
+    answer = []
+    
+    for i in picture:
+        s=''
+        for j in i:
+            s+=j*k
+        for _ in range(k):
+            answer.append(s)
+    return answer
