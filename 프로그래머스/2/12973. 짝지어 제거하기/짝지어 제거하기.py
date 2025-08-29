b820fa97d32f7d72bdd87a27baa2c7798fd6847e
bon24 <gbk24@naver.com>
2025-08-29 14:23:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 163.39 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,11 +1,9 @@
 def solution(s):
     lst = []
-    # lst.append(s[0])
-    # s=s[1:]
     for i in s:
         if len(lst)==0:
             lst.append(i)
-        elif lst[len(lst)-1] == i:
+        elif lst[-1] == i:
             lst.pop()
         else:
             lst.append(i)
