185405a6e43fac0c7665c510d46f7d3b2d564086
bon24 <gbk24@naver.com>
2026-06-06 12:14:30 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,14 +1,10 @@
 def solution(dots):
-    answer = 1
-    side = []
-    for i in range(len(dots)-1):
-        if abs(dots[i][0] - dots[i+1][0])>0:
-            side.append(abs(dots[i][0] - dots[i+1][0]))
-        if abs(dots[i][1] - dots[i+1][1])>0:
-            side.append(abs(dots[i][1] - dots[i+1][1]))
-    side = list(set(side))
-    if len(side) == 1:
-        answer  = side[0]**2
-    else:
-        answer = side[0] * side[1]
+    a =[]
+    b =[]
+    for i,v in dots:
+        a.append(i)
+        b.append(v)
+    a=sorted(list(set(a)))
+    b=sorted(list(set(b)))
+    answer = abs(a[0]-a[1])*abs(b[0]-b[1])
     return answer
