1b44089084a61932f52afb7ced587904bc0d7d55
bon24 <gbk24@naver.com>
2026-06-06 12:57:06 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,9 +1,24 @@
 def solution(score):
     mean = list(map(lambda x: sum(x)/2,score))
-    answer = [1] * len(mean)
+    answer=[1]*len(mean)
     for i in range(len(mean)):
         for j in range(len(mean)):
             if mean[i] < mean[j]:
                 answer[i]+=1
-    
     return answer
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    # mean = list(map(lambda x: sum(x)/2,score))
+    # answer = [1] * len(mean)
+    # for i in range(len(mean)):
+    #     for j in range(len(mean)):
+    #         if mean[i] < mean[j]:
+    #             answer[i]+=1
+    # return answer
