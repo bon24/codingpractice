83f4e5d6f018d56ffd99357a2abf92f137949fae
bon24 <gbk24@naver.com>
2026-06-06 12:26:55 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,10 +1,12 @@
 def solution(n):
-    answer = 0
-    no_three = [] 
-    num = 1
-    while len(no_three) < n :
-        if num % 3!=0 and "3" not in str(num):
-            no_three.append(num)
+    answer=0
+    num=1
+    three=[]
+    while len(three)<n:
+        if num%3!=0 and '3' not in str(num):
+            three.append(num)
         num+=1
-    answer = no_three.pop()
+    answer=three.pop()
+            
     return answer
+    
