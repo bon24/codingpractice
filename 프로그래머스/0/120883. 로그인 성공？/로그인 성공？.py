e451eb09d8f91024bf56609dbbf74340d9f53ffb
bon24 <gbk24@naver.com>
2026-06-06 12:09:08 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,14 +1,13 @@
 def solution(id_pw, db):
-    answer = ''
-    iid,pw = id_pw[0],id_pw[1]
-    for i,p in db:
-        if iid == i and pw == p:
-            answer = "login"
-            break
-        else:
-            if iid ==i and pw != p:
-                answer="wrong pw"
+    answer=''
+    for v in db:
+        if id_pw[0] in v:
+            if id_pw[1] == v[1]:
+                answer='login'
                 break
             else:
-                answer = "fail"
+                answer='wrong pw'
+                break
+        else:
+            answer='fail'
     return answer
