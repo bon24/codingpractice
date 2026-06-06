e7dc7cc66580de19e05ef0e00f03a3ed0e182747
bon24 <gbk24@naver.com>
2026-06-06 13:11:22 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,29 +1,13 @@
 def solution(A, B):
     A = list(A)
-    answer = 0
+    result=-1
     if ''.join(A) == B:
-        return 0
-    
-    while answer<len(B):
-        answer+=1
+            return 0
+    for i in range(0,len(A)+1):
         A.insert(0,A.pop())
-        if B == ''.join(A):
-            break
-    if answer >= len(B):
-        return -1
-    else:
-        return answer
+        if ''.join(A) ==B:
+            return i+1
+    return result
             
             
             
-            
-    # for i in range(len(A)-1,0,-1):
-    #     A.insert(0,A[i])
-    #     print(A)
-    #     print(''.join(A[:len(B)]))
-    #     if B == ''.join(A[:len(B)]):
-    #         chance.append(i)
-    # if len(chance)==0:
-    #     return -1
-    # else:
-    #     return min(chance)
